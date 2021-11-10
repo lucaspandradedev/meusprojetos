5e527b4ed040ef1c3a471596379e41244d6fb090
lucasandrade <lucaspandrade.dev@gmail.com>
2021-11-10 13:24:42 -0300

    Iniciando prijeto AWAX

--- python/PythonExercicios/teste2.py
+++ python/PythonExercicios/teste2.py
@@ -1,4 +1,4 @@
-from turtle import *
+'''from turtle import *
 color('red', 'yellow')
 begin_fill()
 while True:
@@ -8,4 +8,21 @@ while True:
     if abs(pos()) < 1:
         break
 end_fill()
-done()
+done()'''
+
+
+from tkinter import *
+from tkinter import PhotoImage
+
+menu_inicial = Tk()
+
+menu_inicial.title("Primeira Aplicação")
+
+menu_inicial.geometry("500x250+750+400")
+
+menu_inicial.resizable(True, True) #1 e 0 dão o mesmo resultado
+
+#menu_inicial.minsize(500, 250)
+#menu_inicial.maxsize(700, 400)
+
+menu_inicial.mainloop()
