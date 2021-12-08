2fc666cc5c9633feaa11de4cb8aa0ffa35f99806
Lucas Andrade <lucaspandrade.dev@gmail.com>
2021-12-08 15:10:27 -0300

    Atualizando prohetos, inclusive inserindo JavaScript

---
+++ script.js
@@ -0,0 +1,32 @@
+
+/*window.onload = function() {
+    document.getElementById('cabeça').innerHTML = "ooomiiiiiiiiiiiiiiiii";
+    alert('Opa, tudo bom amigão?');
+    document.querySelector('#cabeça').classList.add('mudazul');
+}*/
+
+/*function mostrarTelefone(){
+    document.querySelector('#telefone').style.display = 'block';
+    document.querySelector('.show--button').style.display = 'none';
+}*/
+
+/*let html = '';
+
+let c = 0;
+
+while (c < 10) {
+    html ++ "Número: "+c+"<br/>";
+    c++;
+
+}
+
+
+document.querySelector('.demo').innerHTML = html;
+*/
+
+
+let n = 10;
+
+num = n.toString();
+
+console.log(num);
