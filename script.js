567332637fa92da6863acef0a0ffe642e932d054
Lucas Andrade <lucaspandrade.dev@gmail.com>
2021-12-15 10:40:47 -0300

    Fazendo pequenas atualizações

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
+
+/*let lista1 = [0, 5, 3, 11, 9, 15]
+lista1.sort((a, b) => a - b)
+console.log(lista1)*/
+
+
+
+
+let lista = document.getElementById('novomenu');
+let novoitem = document.createElement('li');
+novoitem.appendChild(document.createTextNode('OLHA EU AQUIIII'))
+lista.appendChild(novoitem)
+
+
+
+
 /*window.onload = function() {
     document.getElementById('cabeça').innerHTML = "ooomiiiiiiiiiiiiiiiii";
     alert('Opa, tudo bom amigão?');
@@ -25,8 +41,19 @@ document.querySelector('.demo').innerHTML = html;
 */
 
 
-let n = 10;
+/*let n = 10;
 
 num = n.toString();
 
-console.log(num);
+console.log(num);*/
+
+
+/*let lista = ["Ovo", "Farinha", "Leite", "Margarina"]
+
+let res = lista.splice(1,2)
+
+console.log(lista)
+console.log('Depois de alterada: '+ lista)*/
+
+
+//se eu tivesse uma segunda lista, poderia usar o .concat para juntar
