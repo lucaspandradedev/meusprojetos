0fe28f39f49d6f648ad69651badcec79502c0470
Lucas Andrade <lucaspandrade.dev@gmail.com>
2021-12-16 14:32:29 -0300

    projetoJulia em andamento

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-
-
 /*let lista1 = [0, 5, 3, 11, 9, 15]
 lista1.sort((a, b) => a - b)
 console.log(lista1)*/
 
-
-
-
-let lista = document.getElementById('novomenu');
+/*let lista = document.getElementById('novomenu');
 let novoitem = document.createElement('li');
 novoitem.appendChild(document.createTextNode('OLHA EU AQUIIII'))
-lista.appendChild(novoitem)
-
-
-
+lista.appendChild(novoitem)*/
 
 /*window.onload = function() {
     document.getElementById('cabeça').innerHTML = "ooomiiiiiiiiiiiiiiiii";
@@ -36,10 +28,8 @@ while (c < 10) {
 
 }
 
-
 document.querySelector('.demo').innerHTML = html;
 */
-
 
 /*let n = 10;
 
@@ -57,3 +47,58 @@ console.log('Depois de alterada: '+ lista)*/
 
 
 //se eu tivesse uma segunda lista, poderia usar o .concat para juntar
+
+/*let timer; 
+
+function comecar(){
+    timer = setInterval(showTime);
+}
+
+function parar(){
+    clearInterval(timer);
+}
+
+
+function showTime(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = h+':'+m+':'+s;
+
+    document.querySelector('.demo').innerHTML = txt
+}
+*/
+
+
+/*let timer;
+
+function rodar(){
+    timer = setTimeout(function(){
+        document.querySelector('.demo').innerHTML = 'Rodoou!    '
+    }, 2000)
+}
+
+function parar(){
+    clearTimeout(timer);
+}*/
+
+
+let pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Pereira',
+    idade: 25,
+    social: {
+        facebook: 'lucaspereira',
+        instagram: '@lpdas_',
+        github: '@lucaspandradedev'
+    },
+    nomeCompleto: function() {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+};
+
+let {nome:nomedomeliante, sobrenome, idade} = pessoa
+
+console.log(nomedomeliante, sobrenome, idade);
+
